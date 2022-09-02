c106610ae1f3e296e4e1783c14d50cb682b325a5
Cyma-s <ssol0319@naver.com>
2022-09-02 11:44:05 +0900

    add : initial commit

---
+++ src/main/java/com/caubogeo/bogeo/BogeoApplication.java
@@ -0,0 +1,13 @@
+package com.caubogeo.bogeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogeoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogeoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caubogeo/bogeo/BogeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caubogeo.bogeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogeoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
