9f41e95535b7e3398b4dd8ee08271988222b6f25
Cyma-s <ssol0319@naver.com>
2022-11-04 01:47:25 +0900

    add : MedicineQuery class for query efficiency

---
+++ src/main/java/com/caubogeo/bogeo/dto/medicine/MedicineQuery.java
@@ -0,0 +1,9 @@
+package com.caubogeo.bogeo.dto.medicine;
+
+public interface MedicineQuery {
+    String getItemSeq();
+    String getItemName();
+    String getMedicineCode();
+    String getImage();
+    String getClassName();
+}
