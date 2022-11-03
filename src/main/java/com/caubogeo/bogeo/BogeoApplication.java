25514b0638dc9cf30c26c5591c73e30615d260c1
Cyma-s <ssol0319@naver.com>
2022-11-03 23:57:19 +0900

    fix : @RequestParam encoding solving

--- src/main/java/com/caubogeo/bogeo/BogeoApplication.java
+++ src/main/java/com/caubogeo/bogeo/BogeoApplication.java
@@ -2,12 +2,21 @@ package com.caubogeo.bogeo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @EnableJpaAuditing
 @SpringBootApplication
 public class BogeoApplication {
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new
+                CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BogeoApplication.class, args);
