302bb3e0ae4b802fd6c2c2443fcfac10982470c2
Cyma-s <ssol0319@naver.com>
2022-11-04 01:50:23 +0900

    fix : change query object

--- src/main/java/com/caubogeo/bogeo/repository/MedicineDetailRepository.java
+++ src/main/java/com/caubogeo/bogeo/repository/MedicineDetailRepository.java
@@ -1,12 +1,13 @@
 package com.caubogeo.bogeo.repository;
 
 import com.caubogeo.bogeo.domain.medicine.MedicineDetail;
-import com.caubogeo.bogeo.dto.medicine.MedicineResponseDto;
+import com.caubogeo.bogeo.dto.medicine.MedicineQuery;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MedicineDetailRepository extends JpaRepository<MedicineDetail, Long> {
-    List<MedicineResponseDto> findByItemNameContains(String itemName);
+
+    List<MedicineQuery> findByItemNameContains(String itemName);
 }
