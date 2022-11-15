41af699a91f6b7f172dc81ee0985e517c4d3ee61
Cyma-s <ssol0319@naver.com>
2022-11-16 01:30:08 +0900

    feat : add error type for NOT_FOUND_MEDICINE

--- src/main/java/com/caubogeo/bogeo/exceptionhandler/MedicineExceptionType.java
+++ src/main/java/com/caubogeo/bogeo/exceptionhandler/MedicineExceptionType.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MedicineExceptionType implements BaseExceptionType {
-    OUT_OF_DATE_END_DATE("OUT_OF_DATE_END_DATE", "끝나는 날짜는 과거의 날짜가 될 수 없습니다.", HttpStatus.BAD_REQUEST);
+    OUT_OF_DATE_END_DATE("OUT_OF_DATE_END_DATE", "끝나는 날짜는 과거의 날짜가 될 수 없습니다.", HttpStatus.BAD_REQUEST),
+    NOT_FOUND_MEDICINE("NOT_FOUND_MEDICINE", "약을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String errorCode;
     private final String message;
