85ce7d23b446826a63f054c0d003824aa99f6585
Cyma-s <ssol0319@naver.com>
2022-11-16 01:30:27 +0900

    feat : add findById method

--- src/main/java/com/caubogeo/bogeo/repository/UserMedicineRepository.java
+++ src/main/java/com/caubogeo/bogeo/repository/UserMedicineRepository.java
@@ -3,10 +3,12 @@ package com.caubogeo.bogeo.repository;
 import com.caubogeo.bogeo.domain.member.Medicine;
 import com.caubogeo.bogeo.domain.member.Member;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserMedicineRepository extends JpaRepository<Medicine, Long> {
     List<Medicine> findByUser(Member user);
+    Optional<Medicine> findById(Long id);
 }
