4022aac9f7346fd7385c8eb2e09923d15f6398e3
Cyma-s <ssol0319@naver.com>
2022-11-16 01:31:41 +0900

    feat : add api that changes activation of user's medicine

--- src/main/java/com/caubogeo/bogeo/controller/user/UserMedicineController.java
+++ src/main/java/com/caubogeo/bogeo/controller/user/UserMedicineController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +40,13 @@ public class UserMedicineController {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
         return new ResponseEntity<>(userMedicineService.getUserMedicines(id, year, month, day), headers, HttpStatus.OK);
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> changeMedicineActivation(@RequestParam("id") Long id) {
+        userMedicineService.changeMedicineActivation(id);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", StandardCharsets.UTF_8));
+        return new ResponseEntity<>(headers, HttpStatus.OK);
     }
 }
