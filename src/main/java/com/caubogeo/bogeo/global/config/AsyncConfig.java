7e28bc4ab46f6ce5d108cf2a67ec4fc961bde544
Cyma-s <ssol0319@naver.com>
2022-12-08 00:21:26 +0900

    feat: add AsyncConfig configuration

---
+++ src/main/java/com/caubogeo/bogeo/global/config/AsyncConfig.java
@@ -0,0 +1,22 @@
+package com.caubogeo.bogeo.global.config;
+
+import java.util.concurrent.Executor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    @Bean("scheduleAsyncExecutor")
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(50);
+        executor.setQueueCapacity(100);
+        executor.setThreadNamePrefix("schedule-");
+        executor.initialize();
+        return executor;
+    }
+}
